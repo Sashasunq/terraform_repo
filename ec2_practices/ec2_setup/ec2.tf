5123cd3f513f538071b9b4256bec32441b8a2cea
sashasunq <sashasunq25@gmail.com>
2018-10-29 15:07:36 +0200

    complete changes for ec2 and default vpc

--- ec2_practices/ec2_setup/ec2.tf
+++ ec2_practices/ec2_setup/ec2.tf
@@ -1,26 +1,8 @@
 provider "aws" {
-  region = "${var.region1}"
+  region = "${var.region}"
 }
 
 resource "aws_instance" "ami" {
-  ami           = "ami-063aa838bd7631e0b"
-  instance_type = "t2.micro"
-}
-
-provider "aws" {
-  region = "${var.region2}"
-}
-
-resource "aws_instance" "ami" {
-  ami           = "ami-0ac019f4fcb7cb7e6"
-  instance_type = "t2.micro"
-}
-
-provider "aws" {
-  region = "${var.region2}"
-}
-
-resource "aws_instance" "ami" {
-  ami           = ""
-  instance_type = "t2.micro"
+  ami            = "ami-0bdf93799014acdc4"
+  instance_type  = "t2.micro"
 }
