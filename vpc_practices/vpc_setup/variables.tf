8c74013e81a26aab6bf2e1396b86ad8b76603c43
sashasunq <sashasunq25@gmail.com>
2018-10-29 15:32:28 +0200

    variables added

--- vpc_practices/vpc_setup/variables.tf
+++ vpc_practices/vpc_setup/variables.tf
@@ -1,3 +1,22 @@
-variable "region" {
-  default = "eu-central-1"
+variable "aws_region" {
+  default = "eu-west-3"
+}
+
+# Production
+
+variable "vpc_prod_cidr" {
+  description = "CIDR for the prod VPC"
+  default     = "172.32.0.0/16"
+}
+
+variable "vpc_prod_cidr_region_a" {
+  default = "172.32.0.0/24"
+}
+
+variable "vpc_prod_cidr_region_b" {
+  default = "172.32.1.0/24"
+}
+
+variable "vpc_prod_cidr_region_c" {
+  default = "172.32.2.0/24"
 }
